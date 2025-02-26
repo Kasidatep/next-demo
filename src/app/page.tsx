3348c503edf6feae9afa48605f96f2447f05a2ba
Kasidate P <bank@kasidate.me>
2025-02-26 14:21:13 +0700

    feat: Enhance authentication flow with consent checkbox and update environment variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container m-5">
-      <div className="font-bold text-xl">Home</div>
-      <Link href={"/captcha"}> Captcha</Link>
-      <Link href={"/todo"}> Todo</Link>
+    <div className="container m-5  flex flex-col ">
+      <div className="font-bold text-xl">KasidatePLUS Demo Website - Menu</div>
+      <Link href={"/captcha"}>&gt;` Captcha</Link>
+      <Link href={"/todo"}>&gt;` Todo</Link>
+      <Link href={"/auth/signin"}>&gt;` I&apos;m Guide Log In</Link>
+
     </div>
   );
 }
--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-
 import { useEffect } from 'react';
 import { useSession, signOut } from 'next-auth/react';
 import { NextPage } from 'next';
@@ -9,35 +8,34 @@ const ProtectedPage: NextPage = () => {
   const { data: session } = useSession();
 
   useEffect(() => {
-    // if (session?.user.idToken) {
-    //   fetch('/api/your-backend-endpoint', {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //       'Authorization': `Bearer ${session.user.idToken}`,
-    //     },
-    //     body: JSON.stringify({ token: session.user.idToken }),
-    //   })
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     console.log('Success:', data);
-    //   })
-    //   .catch((error) => {
-    //     console.error('Error:', error);
-    //   });
-    // }
-
     console.log(session);
   }, [session]);
 
   if (!session) {
-    return <p>Loading...</p>;
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <p className="text-lg text-gray-700">Loading...</p>
+      </div>
+    );
   }
 
   return (
-    <div>
-      <h1>Protected Page</h1>
-      <button onClick={() => signOut()}>Sign out</button>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">Protected Page</h1>
+
+      <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-md">
+        <h2 className="text-xl font-semibold text-gray-700 mb-2">Session Data:</h2>
+        <pre className="bg-gray-200 p-4 rounded-md text-sm text-gray-800 overflow-auto">
+          {JSON.stringify(session, null, 2)}
+        </pre>
+      </div>
+
+      <button
+        onClick={() => signOut()}
+        className="mt-6 px-6 py-2 bg-red-500 text-white font-medium rounded-lg hover:bg-red-600 transition"
+      >
+        Sign out
+      </button>
     </div>
   );
 };
